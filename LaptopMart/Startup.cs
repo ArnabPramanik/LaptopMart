715f0d7777ba7d446d70d0e3d441cd775c88153b
Arnab <arnab_pmk@yahoo.com>
2017-12-19 20:10:06 +0530

    Add project files.

---
+++ LaptopMart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LaptopMart.Startup))]
+namespace LaptopMart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
