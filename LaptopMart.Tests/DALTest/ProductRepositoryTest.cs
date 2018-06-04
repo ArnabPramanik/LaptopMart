ce47f2dee5aa2885ee9c610f782e1d831b073406
Arnab <arnab_pmk@yahoo.com>
2018-06-04 23:01:35 +0530

    NOt compiling

---
+++ LaptopMart.Tests/DALTest/ProductRepositoryTest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LaptopMart.Tests.DALTest
+{
+    [TestClass]
+    public class ProductRepositoryTest
+    {
+    }
+}
---
+++ LaptopMart.Tests/DALTests/ProductRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using LaptopMart.ApplicationDb;
+using LaptopMart.Implementations;
+using LaptopMart.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Data.Entity;
+namespace LaptopMart.Tests.DALTests
+{
+    public class ProductRepositoryTest
+    {
+        private ProductRepository _repository;
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var mockProducts = new Mock<DbSet<Product>>();
+            var mockContext = new Mock<IApplicationDbContext>();
+            mockContext.SetupGet(c => c.ProductTable).Returns(mockProducts.Object);
+            _repository = new ProductRepository(mockContext.Object);
+        }
+    }
+
+}
